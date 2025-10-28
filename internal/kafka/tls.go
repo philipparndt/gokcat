b18b0acd294fb98e37a79339b87a57de2ca9a413
Philipp Arndt <philipp.arndt@vector.com>
2025-10-28 13:34:02 +0100

    feat: ability to skip root ca configuration

--- internal/kafka/tls.go
+++ internal/kafka/tls.go
@@ -14,20 +14,24 @@ func NewTLSConfig(certFile, keyFile, caFile string, insecure bool) (*tls.Config,
 		return nil, err
 	}
 
-	// Load CA cert
-	caCertPool := x509.NewCertPool()
-	caCertBytes, err := os.ReadFile(caFile)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read CA certificate: %w", err)
-	}
-	if ok := caCertPool.AppendCertsFromPEM(caCertBytes); !ok {
-		return nil, errors.New("failed to append CA certificate to pool")
-	}
-
-	return &tls.Config{
+	config := &tls.Config{
 		MinVersion:         tls.VersionTLS12,
 		Certificates:       []tls.Certificate{cert},
-		RootCAs:            caCertPool,
 		InsecureSkipVerify: insecure,
-	}, nil
+	}
+
+	if caFile != "" {
+		caCertBytes, err := os.ReadFile(caFile)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read CA certificate: %w", err)
+		}
+		caCertPool := x509.NewCertPool()
+		if ok := caCertPool.AppendCertsFromPEM(caCertBytes); !ok {
+			return nil, errors.New("failed to append CA certificate to pool")
+		}
+
+		config.RootCAs = caCertPool
+	}
+
+	return config, nil
 }
